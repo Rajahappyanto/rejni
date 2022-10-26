152b325c82a40766fbb918ee69433a6598874bd8
luffy <rajadesign80@gmail.com>
2022-10-26 11:13:09 +0700

    ygh

---
+++ practice.py
@@ -0,0 +1,13 @@
+suka_pedas=input("Suka pedas(Y/T)?")
+tanggal_tua=("Tanggal tua(Y/T)?")
+
+if suka_pedas=="Y":
+ if tanggal_tua=="Y":
+  print("Rekomendasi menu: Nasi sambal")
+ else:
+ 	print("Rekomendasi menu: Nasi rica-rica iga sapi")
+else:
+   if tanggal_tua=="Y":
+    print("Rekomendasi menu:Nasi Kecap")
+   else:
+   	print("Recomendasi menu:Nasi ayam kecap")
